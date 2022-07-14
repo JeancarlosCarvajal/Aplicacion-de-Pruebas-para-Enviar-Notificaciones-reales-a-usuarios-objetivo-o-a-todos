5e7ac7cde8a3c17320decbc225eaef857da007d4
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-07-13 20:01:43 -0400

    Pasntallas Home y Mensajes Listas

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notificaciones/screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'home',
+      routes: {
+        'home' : ( _ ) => const HomeScreen(),
+        'message' : ( _ ) => const MessageScreen(), 
+      },
     );
   }
 }
